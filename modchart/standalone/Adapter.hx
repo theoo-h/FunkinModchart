740d948c42e467bdba10eb07ce719f9c424d10e6
Furo <furogaming9@gmail.com>
2025-02-18 23:43:35 +0000

    Debug logs being "FM_VERBOSE" and better logs prefix to indicate the user and of course fixed vocabulary

--- modchart/standalone/Adapter.hx
+++ modchart/standalone/Adapter.hx
@@ -1,20 +1,28 @@
 package modchart.standalone;
 
+import haxe.macro.Compiler;
+
 class Adapter {
 	public static var instance:IAdapter;
-	private static var ENGINE_NAME:String = haxe.macro.Compiler.getDefine("FM_ENGINE");
+	private static var ENGINE_NAME:String = Compiler.getDefine("FM_ENGINE");
 
 	public static function init() {
 		if (instance != null)
 			return;
 
 		final possibleClientName = ENGINE_NAME.substr(0, 1).toUpperCase() + ENGINE_NAME.substr(1).toLowerCase();
-		final adapter = Type.createInstance(Type.resolveClass('modchart.standalone.adapters.${ENGINE_NAME.toLowerCase()}.' + possibleClientName), []);
+		final adapter = Type.createInstance(Type.resolveClass('modchart.standalone.adapters.${ENGINE_NAME.toLowerCase()}.${possibleClientName}'), []);
 
-		trace('modchart.standalone.adapters.${ENGINE_NAME.toLowerCase()}.' + possibleClientName);
+		#if FM_VERBOSE
+		trace('[FunkinModchart Verbose] Finding possible adapter from "modchart.standalone.adapters.${ENGINE_NAME.toLowerCase()}.${possibleClientName}"');
+		#end
 
 		if (adapter == null)
-			throw 'Adapter not founded for $ENGINE_NAME';
+			throw 'Adapter not found for $ENGINE_NAME';
+
+		#if FM_VERBOSE
+		trace('[FunkinModchart Verbose] Found Adapter!');
+		#end
 
 		instance = adapter;
 	}
